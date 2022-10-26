e7eb89f55177dbfd23065fed552cab0db2bb21fa
adamzterenyi <112423622+adamzterenyi@users.noreply.github.com>
2022-10-25 19:28:52 -0700

    upload of visualization

---
+++ README.md
@@ -0,0 +1,8 @@
+# Visualizing Interesting Datasets
+For [project_02](https://github.com/mikeizbicki/cmc-csci040/tree/2022fall/project_02), I plotted two graphs *(one from two JSON datasets and another from one CSV dataset)* using python and MatPlotLib.
+
+## US and Indian GDP figures from 1960 to 2021
+![](USandIndiaGDPfigs.png)
+
+## Number of Nobel Laureates Born in the Present Day Borders of Founding EU Countries from 1901-2016
+![](nobellaureatesEUfounders.png)
---
+++ datasets.py
@@ -0,0 +1,137 @@
+import json
+import matplotlib.pyplot as plt
+
+# JSON time
+'''
+# first try, hated this
+belgium = 'BE'
+france = 'FR'
+germany = 'DE'
+italy = 'IT'
+luxembourg = 'LU'
+netherlands = 'NL'
+
+be =[]
+fr = []
+de = []
+it = []
+lu = []
+nl = []
+laureates = []
+
+with open('project_02/laureate.json', encoding = 'utf-8') as f:
+    text = f.read()
+    laureates += json.loads(text)
+    actuallaureates = laureates[0]
+print(laureates)
+print(len(actuallaureates))
+'''
+
+igdpfigs = []
+# JSON second try, but with India GDP
+paths = ['project_02/indiagdp.json']
+for path in paths:
+    with open(path, encoding = 'ascii') as f:
+        text = f.read()
+        igdpfigs += json.loads(text)
+    iactualgdpfigs =  igdpfigs[1]
+
+igdpyears = []
+for set in iactualgdpfigs:
+    gdpyear = int(set['date'])
+    igdpyears.append(gdpyear)
+igdpyears.sort()
+
+igdpnumbers = []
+for set in iactualgdpfigs:
+    gdpnumber = int(set['value'])/1000000000
+    igdpnumbers.append(gdpnumber)
+igdpnumbers = igdpnumbers[::-1]
+
+# to check for parity and accuracy
+'''
+print('india years=', igdpyears)
+print('india gdp/year=', igdpnumbers)
+print(len(str(igdpnumbers[-1])))
+'''
+
+# JSON continued, add in the US
+ugdpfigs = []
+paths = ['project_02/usagdp.json']
+for path in paths:
+    with open(path, encoding = 'ascii') as f:
+        text = f.read()
+        ugdpfigs += json.loads(text)
+    uactualgdpfigs = ugdpfigs[1]
+
+ugdpyears = []
+for set in uactualgdpfigs:
+    gdpyear = int(set['date'])
+    ugdpyears.append(gdpyear)
+ugdpyears.sort()
+
+ugdpnumbers = []
+for set in uactualgdpfigs:
+    gdpnumber = int(set['value'])/1000000000
+    ugdpnumbers.append(gdpnumber)
+ugdpnumbers = ugdpnumbers[::-1]
+
+# to check for parity and accuracy
+'''
+print('usa years=', ugdpyears)
+print('usa gdp/year=', ugdpnumbers)
+print(len(str(ugdpnumbers[-1])))
+'''
+
+# plotting time.
+plt.plot(ugdpyears, ugdpnumbers, color = 'blue', label = 'USA')
+plt.plot(igdpyears, igdpnumbers, color = 'orange', label = 'India')
+'''plt.title('US and Indian GDP figures from 1960 to 2021')'''
+plt.xlabel('Years')
+plt.ylabel('GDP in USD$, billions')
+plt.yticks(range(0, 24000, 2000))
+plt.legend(loc='upper left')
+plt.show()
+
+# CSV time
+import csv
+
+nobellaureates = open('project_02/laureate.csv')
+laureatesReader = csv.reader(nobellaureates)
+laureates = list(laureatesReader)
+print(laureates[940][6])
+
+# European Coal and Steel Community (European Union) founding countries
+be = 0
+fr = 0
+de = 0
+it = 0
+lu = 0
+nl = 0
+
+for laureate in laureates:
+    if laureate[6] == 'BE':
+        be += 1
+    if laureate[6] == 'FR':
+        fr += 1
+    if laureate[6] == 'DE':
+        de += 1
+    if laureate[6] == 'IT':
+        it += 1
+    if laureate[6] == 'LU':
+        lu += 1
+    if laureate[6] == 'NL':
+        nl += 1
+
+# to check
+'''
+print(be, fr, de, it, lu, nl)
+'''
+
+# plotting time!
+eufounders = ['Belgium', 'France', 'Germany', 'Luxembourg', 'Italy', 'Netherlands']
+nolaureates = [be, fr, de, lu, it, nl]
+plt.xlabel('European Union Founding Countries')
+plt.ylabel('Nobel Laureates Born in Present Day Boundaries')
+plt.bar(eufounders, nolaureates, color = 'orange', width = 0.4)
+plt.show()
